765110e512eea1540ac127d7918fead668dc0311
rishav <rishavjais456@gmail.com>
2025-01-05 13:05:32 +0530

    adding features of showing person name who is typing

--- public/client.js
+++ public/client.js
@@ -1,53 +1,38 @@
-const socket = io()
-let name;
-let textarea = document.querySelector('#textarea')
-let messageArea = document.querySelector('.message__area')
-do {
-    name = prompt('Please enter your name: ')
-} while(!name)
+const socket = io();
+let name = prompt('Please enter your name: ');
+let textarea = document.querySelector('#textarea');
+let messageArea = document.querySelector('.message__area');
+let typingStatus = document.querySelector('#typingStatus');
+let typingTimeout;
 
+// Listen for 'keyup' event to detect typing
 textarea.addEventListener('keyup', (e) => {
-    if(e.key === 'Enter') {
-        sendMessage(e.target.value)
+    if (e.key === 'Enter') {
+        sendMessage(e.target.value);
+    } else {
+        socket.emit('typing', { name, isTyping: true });
+        clearTimeout(typingTimeout);
+        typingTimeout = setTimeout(() => socket.emit('typing', { name, isTyping: false }), 1000);
     }
-})
+});
 
+// Send message to the server
 function sendMessage(message) {
-    let msg = {
-        user: name,
-        message: message.trim()
-    }
-    // Append 
-    appendMessage(msg, 'outgoing')
-    textarea.value = ''
-    scrollToBottom()
-
-    // Send to server 
-    socket.emit('message', msg)
-
+    let msg = { user: name, message: message.trim() };
+    appendMessage(msg, 'outgoing');
+    textarea.value = '';
+    socket.emit('message', msg);
 }
 
+// Append message to the chat
 function appendMessage(msg, type) {
-    let mainDiv = document.createElement('div')
-    let className = type
-    mainDiv.classList.add(className, 'message')
-
-    let markup = `
-        <h4>${msg.user}</h4>
-        <p>${msg.message}</p>
-    `
-    mainDiv.innerHTML = markup
-    messageArea.appendChild(mainDiv)
+    let div = document.createElement('div');
+    div.classList.add(type, 'message');
+    div.innerHTML = `<h4>${msg.user}</h4><p>${msg.message}</p>`;
+    messageArea.appendChild(div);
+    messageArea.scrollTop = messageArea.scrollHeight;
 }
 
-// Recieve messages 
-socket.on('message', (msg) => {
-    appendMessage(msg, 'incoming')
-    scrollToBottom()
-})
-
-function scrollToBottom() {
-    messageArea.scrollTop = messageArea.scrollHeight
-}
-
-
+// Listen for incoming messages and typing status
+socket.on('message', (msg) => appendMessage(msg, 'incoming'));
+socket.on('typing', (data) => typingStatus.innerHTML = data.isTyping ? `${data.name} is typing...` : '');
--- server.js
+++ server.js
@@ -1,26 +1,35 @@
-const express = require('express')
-const app = express()
-const http = require('http').createServer(app)
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
 
-const PORT = process.env.PORT || 3000
-                                                                          
+const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})
+    console.log(`Listening on port ${PORT}`);
+});
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+    res.sendFile(__dirname + '/index.html');
+});
 
-// Socket 
-const io = require('socket.io')(http)
+// Socket
+const io = require('socket.io')(http);
 
 io.on('connection', (socket) => {
-    console.log('Connected...')
+    console.log('A user connected...');
+    
+    // Emit the 'message' event to all clients
     socket.on('message', (msg) => {
-        socket.broadcast.emit('message', msg)
-    })
-
-})
+        socket.broadcast.emit('message', msg);
+    });
+    
+    // Handle typing status with the user's name
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', data); // Broadcast typing status and username to others
+    });
+    
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
